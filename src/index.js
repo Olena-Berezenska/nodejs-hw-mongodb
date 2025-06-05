7c1d2cc16088f074f1fcd60aab043a29ddfe916f
OlenaB <elena.yefimchuk@gmail.com>
2025-06-05 15:29:09 -0600

    step 3-6

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+import dotenv from 'dotenv';
 
-setupServer();
+dotenv.config();
+
+// console.log('DEBUG: process.env =', process.env);
+// console.log('NODE_ENV:', process.env.NODE_ENV);
+
+const bootStrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+bootStrap();
