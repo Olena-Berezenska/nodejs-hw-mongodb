e51b2abac9913311a6c28120ae0e7a582de77005
OlenaB <elena.yefimchuk@gmail.com>
2025-06-19 16:47:50 -0600

    finish

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -8,3 +8,26 @@ export const getContactsById = async (contactId) => {
   const contact = await contactsCollection.findById(contactId);
   return contact;
 };
+export const createContact = async (payload) => {
+  const newcontact = await contactsCollection.create(payload);
+  return newcontact;
+};
+export const deleteContact = async (contactId) => {
+  const contact = await contactsCollection.findOneAndDelete({
+    _id: contactId,
+  });
+  return contact;
+};
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await contactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    contact: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
